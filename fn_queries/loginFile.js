89a455f575a1a235a9399d88875c6a1e7c94cfa2
ReneRuizEsc <reneruizesc@gmail.com>
2023-01-10 00:32:49 -0600

    login

--- fn_queries/loginFile.js
+++ fn_queries/loginFile.js
@@ -23,7 +23,7 @@ const loginFn = (req, res, client) => {
       pgp_sym_decrypt(tipodesangre::bytea, $3) as tipodesangre,
       idusuario, fechanac, hasmembresia
       FROM usuario 
-      INNER JOIN cuenta ON idcuenta = idcuenta_fk AND Correo = $1 AND Contrasena = $2
+      INNER JOIN cuenta ON idcuenta = idcuenta_fk AND Correo = $1 AND Contrasena = pgp_sym_encrypt( $2, $3)
       ;
       `;
 
